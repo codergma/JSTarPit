f96ac17506790e75579520dda01442a96aeaeb57
codergma <codergma@gmail.com>
2015-12-14 23:34:56 +0800

    first

---
+++ base.js
@@ -0,0 +1,13 @@
+// js typeof操作符　判断原始数据类型
+typeof myValue === "string";
+typeof myValue === "number";
+typeof myValue === "boolean";
+typeof myValue === "undefined";
+// js 判断null
+myValue === null;
+// jQuery 
+jQuery.isFunction();
+jQuery.isPlainObject();
+jQuery.isArray();
+
+
